fad358f6a859507d6943ac2a86b9c72a051a3a8a
a-harutyunyan <a_harutyunyan@outlook.com>
2023-05-22 22:26:20 +0400

    added

---
+++ YouTubeServiceFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+
+namespace YouTubeMigrationClient
+{
+    public class YouTubeServiceFactory
+    {
+        public static async Task<UserCredential> CreateCredential(string credentialFileName)
+        {
+            UserCredential credential;
+            using (var stream = new FileStream(credentialFileName, FileMode.Open, FileAccess.Read))
+            {
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    (await GoogleClientSecrets.FromStreamAsync(stream)).Secrets,
+                    new[] { YouTubeService.Scope.Youtube },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(Assembly.GetExecutingAssembly().GetType().ToString())
+                );
+            }
+            return credential;
+        }
+    }
+}
